08ec18d1bf069f881d1b5bf6e0bc1577350b061b
hissonew32 <developerjvc@hissoconsultores.com>
2023-09-20 22:57:48 -0500

    ya funciona con vite pero tengo q usar server de laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import {createApp} from 'vue'
+
+import App from './src/App.vue'
+
+createApp(App).mount("#app")
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Laravel">
+	<title>Laravel</title>
+
+	<!-- Fonts -->
+	<link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+	@vite('resources/css/app.css')
+</head>
+
+<body>
+	<div id="app"></div>
+
+	@vite('resources/js/app.js')
+</body>
+</html>
